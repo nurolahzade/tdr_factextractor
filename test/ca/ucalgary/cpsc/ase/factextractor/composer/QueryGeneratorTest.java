170fc5c83becf9902b7d55c4ea375dbc21c0041f
mehrdad <mnurolah@ucalgary.ca>
2012-08-20 16:28:07 -0600

    Printing out heuristic names for result sets

--- test/ca/ucalgary/cpsc/ase/factextractor/composer/QueryGeneratorTest.java
+++ test/ca/ucalgary/cpsc/ase/factextractor/composer/QueryGeneratorTest.java
@@ -2,11 +2,13 @@ package ca.ucalgary.cpsc.ase.factextractor.composer;
 
 import java.io.File;
 import java.util.Map;
+import java.util.Set;
 
 import org.apache.log4j.Logger;
 
 import ca.ucalgary.cpsc.ase.FactManager.entity.TestMethod;
 import ca.ucalgary.cpsc.ase.FactManager.service.TestMethodService;
+import ca.ucalgary.cpsc.ase.QueryManager.Heuristic;
 import ca.ucalgary.cpsc.ase.QueryManager.Query;
 import ca.ucalgary.cpsc.ase.QueryManager.VotingHeuristicManager;
 import ca.ucalgary.cpsc.ase.factextractor.composer.QueryGenerator;
@@ -29,19 +31,32 @@ public class QueryGeneratorTest {
 						
 		QueryGenerator generator = new QueryGenerator();
 		Query query = generator.generate(file);
+		
+		System.out.println(query);
 
 		VotingHeuristicManager manager = new VotingHeuristicManager();
-		Map<Integer, Double> results = manager.match(query);
+		Map<Integer, Set<Heuristic>> results = manager.match(query);
 		
 		print(results);
 	}
 
-	private void print(Map<Integer, Double> results) {
+	private void print(Map<Integer, Set<Heuristic>> results) {
 		TestMethodService service = new TestMethodService();
 		for (Integer id : results.keySet()) {
 			TestMethod tm = service.find(id);
-			System.out.println(id + ", " + tm.getClazz().getFqn() + "." + tm.getName() + "(), " + results.get(id));
+			System.out.println(id + ", " + tm.getClazz().getFqn() + "." + tm.getName() + "(), " + generateHeuristics(results.get(id)));
 		}
+	}
+	
+	private String generateHeuristics(Set<Heuristic> heuristics) {
+		StringBuilder builder = new StringBuilder();
+		builder.append("{");
+		for (Heuristic heuristic : heuristics) {
+			builder.append(heuristic.getName());
+			builder.append(", ");
+		}
+		builder.append("}");
+		return builder.toString();
 	}
 	
 }
